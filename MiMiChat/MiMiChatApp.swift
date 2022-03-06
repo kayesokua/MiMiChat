4e03eb426a6538fce302c227c6e1f36319a2f275
kayesokua <kaye.sokua@code.berlin>
2022-03-06 14:16:29 +0100

    Initial Commit

---
+++ MiMiChat/MiMiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiMiChatApp.swift
+//  MiMiChat
+//
+//  Created by Kaye on 06.03.22.
+//
+
+import SwiftUI
+
+@main
+struct MiMiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
